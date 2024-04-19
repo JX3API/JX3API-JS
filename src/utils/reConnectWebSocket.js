eb840348d0db59282cf304d60abf90abbb037fea
fine <956168905@qq.com>
2024-04-19 12:32:15 +0800

    fix；优化ws

--- src/utils/reConnectWebSocket.js
+++ src/utils/reConnectWebSocket.js
@@ -1,48 +1,36 @@
 import WebSocket from "ws";
-/**
- * 创建一个 reConnectWebSocket 实例。
- * @param {string} url WebSocket 服务器的 URL。
- * @param {object} options WebSocket 配置
- * @param {number} [heartbeatInterval=5000] 发送心跳消息的间隔（毫秒）。
- * @param {number} [reconnectInterval=10000] 重新连接尝试的间隔（毫秒）。
- */
+
 export class reConnectWebSocket {
   url = null;
   options = null;
-  heartbeatInterval = 5000;
-  reconnectInterval = 10000;
+  heartbeatInterval = 5000; // 心跳间隔，默认为5秒
+  reconnectInterval = 10000; // 重新连接间隔，默认为10秒
   ws = null;
   heartbeatTimer = null;
   reconnectTimer = null;
-  isReconnecting = false;
-  connectionAttempts = 0;
-  messageCallBack = [];
+  isReconnecting = false; // 是否正在重新连接
+  connectionAttempts = 0; // 连接尝试次数
+  messageCallBack = []; // 消息回调函数数组
+
   constructor(url, options = undefined, heartbeatInterval = 5000, reconnectInterval = 10000) {
     this.url = url;
-    this.options = options; //连接参数
-    this.heartbeatInterval = heartbeatInterval; //心跳间隔
-    this.reconnectInterval = reconnectInterval; //重新连接间隔
-    this.ws = null; //ws实例
-    this.heartbeatTimer = null; //心跳定时器
-    this.reconnectTimer = null; //重新连接定时器
-    this.isReconnecting = false; //是否正在重新连接
-    this.connectionAttempts = 0; //连接尝试次数
+    this.options = options;
+    this.heartbeatInterval = heartbeatInterval;
+    this.reconnectInterval = reconnectInterval;
   }
 
-  /**
-   * 连接到 WebSocket 服务器。
-   */
+  // 连接 WebSocket
   connect() {
     this.ws = new WebSocket(this.url, this.options);
     this.ws.on("open", () => {
       console.log("WebSocket 已连接");
       this.startHeartbeat();
-      this.connectionAttempts = 0; // 在成功连接时重置连接尝试次数
+      this.connectionAttempts = 0;
     });
 
     this.ws.on("message", (data) => {
       const msg = JSON.parse(data);
-      console.log("收到消息:", msg);
+      console.log("接收到消息:", msg);
       this.messageCallBack.forEach((cb) => {
         cb(msg);
       });
@@ -54,23 +42,18 @@ export class reConnectWebSocket {
     });
 
     this.ws.on("close", () => {
-      console.log("WebSocket 已关闭");
+      console.log("WebSocket 连接关闭");
       this.stopHeartbeat();
       this.reconnect();
     });
   }
 
-  /**
-   * 尝试重新连接到 WebSocket 服务器。
-   */
+  // 重新连接 WebSocket
   reconnect() {
     if (!this.isReconnecting) {
       this.isReconnecting = true;
-      const delay = Math.min(
-        this.reconnectInterval * Math.pow(2, this.connectionAttempts),
-        60000 // 将重新连接尝试间隔上限设置为 1 分钟
-      );
-      console.log(`将在 ${delay / 1000} 秒后尝试重新连接...`);
+      const delay = Math.min(this.reconnectInterval * Math.pow(2, this.connectionAttempts), 60000);
+      console.log(`将在 ${delay / 1000} 秒后重新连接...`);
       this.reconnectTimer = setTimeout(() => {
         this.connect();
         this.isReconnecting = false;
@@ -79,37 +62,41 @@ export class reConnectWebSocket {
     }
   }
 
-  /**
-   * 开始发送心跳消息。
-   */
+  // 开始心跳检测
   startHeartbeat() {
-    this.heartbeatTimer = setInterval(() => {
-      if (this.ws.readyState === WebSocket.OPEN) {
-        // console.log("发送心跳");
-        this.ws.ping();
-      } else {
-        this.reconnect(); // 如果 WebSocket 不处于打开状态，则重新连接
+    let consecutivePingFailures = 0;
+
+    const sendPing = () => {
+      if (consecutivePingFailures >= 3) {
+        console.log(`心跳失败次数：${consecutivePingFailures} 重新连接`);
+        this.reconnect();
+        return;
       }
-    }, this.heartbeatInterval);
+      if (this.ws.readyState === WebSocket.OPEN) {
+        this.ws.ping();
+        // console.log(`发送ping 失败次数：${consecutivePingFailures}`);
+        consecutivePingFailures++;
+      } else {
+        this.reconnect();
+      }
+    };
+
+    this.heartbeatTimer = setInterval(sendPing, this.heartbeatInterval);
 
     this.ws.on("pong", () => {
-      // console.log("收到心跳响应");
+      // console.log("收到pong");
+      consecutivePingFailures = 0;
     });
 
-    this.ws.ping(); // 发送初始 ping
+    sendPing(); // 发送初始 ping
   }
 
-  /**
-   * 停止发送心跳消息。
-   */
+  // 停止心跳检测
   stopHeartbeat() {
     clearInterval(this.heartbeatTimer);
   }
 
-  /**
-   * 发送消息到 WebSocket 服务器。
-   * @param {string} message 要发送的消息。
-   */
+  // 发送消息
   send(message) {
     if (this.ws.readyState === WebSocket.OPEN) {
       this.ws.send(message);
@@ -118,17 +105,17 @@ export class reConnectWebSocket {
     }
   }
 
-  /**
-   * 关闭 WebSocket 连接。
-   */
+  // 关闭连接
   close() {
     clearTimeout(this.reconnectTimer);
     this.stopHeartbeat();
     this.ws.close();
   }
+
+  // 添加消息回调函数
   message(callback) {
     if (typeof callback !== "function") {
-      throw new Error("message回调必须是函数");
+      throw new Error("消息回调必须是一个函数");
     }
     this.messageCallBack.push(callback);
   }
